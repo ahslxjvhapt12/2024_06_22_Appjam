6817615e89662762d63ecd31a247967409376a09
Schokolade796 <dmsdn796@gmail.com>
2024-06-23 07:49:17 +0900

    D

---
+++ Appjam/Assets/02_Scripts/Player/Star.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Star : MonoBehaviour
+{
+    private bool start = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Image>().enabled == true && start)
+        {
+            GetComponent<AudioSource>().Play();
+            start = false;
+        }
+        else if(GetComponent<Image>().enabled == false && !start)
+        {
+            start = true;
+        }
+    }
+}
