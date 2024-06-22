f4351ffbfb4c9ba0c33aefcf3dc3fe3eaa766fb4
ahslxjvhapt12 <likjhee1@gmail.com>
2024-06-22 18:14:34 +0900

    Import TextMeshPro

---
+++ Appjam/Assets/02_Scripts/Weapon/Weapons/Pistol.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Attack(Transform target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
