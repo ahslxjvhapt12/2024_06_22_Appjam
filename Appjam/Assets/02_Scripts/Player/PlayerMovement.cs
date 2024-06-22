fac96c4ee44ae3bec1e69e22a9cdc6eba2564b6e
ahslxjvhapt12 <likjhee1@gmail.com>
2024-06-22 20:29:55 +0900

    Animation

--- Appjam/Assets/02_Scripts/Player/PlayerMovement.cs
+++ Appjam/Assets/02_Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,8 +10,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float HP = 10f;
     [SerializeField] private float hitDelay = 1.5f;
 
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    private readonly int walkHash = Animator.StringToHash("Walk");
+
     private bool isHit = false;
     private bool isdeath = false;
+
+    private void Awake()
+    {
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
 
     private void Update()
     {
@@ -19,13 +33,19 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(h, v, 0).normalized * speed * Time.deltaTime;
+        Vector3 moveVector = new Vector3(h, v, 0).normalized;
+        transform.position += moveVector * speed * Time.deltaTime;
 
+        animator.SetBool(walkHash, moveVector != Vector3.zero);
+        if (h != 0)
+        {
+            spriteRenderer.flipX = h < 0;
+        }
     }
 
     public void Hit(float damage)
     {
-        if(isHit || isdeath) return;
+        if (isHit || isdeath) return;
         else
         {
             IsHit();
@@ -41,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
     {
         isHit = !isHit;
 
-        if(isHit) GetComponent<SpriteRenderer>().color = new Color(1,1,1,0.5f);
-        else GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
+        if (isHit) GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        else GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
     }
 }
