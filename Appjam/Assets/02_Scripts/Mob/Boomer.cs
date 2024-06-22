ecfcab8bda8edb177b262ddfbfda9c9922b8dfb8
Schokolade796 <dmsdn796@gmail.com>
2024-06-22 23:49:29 +0900

    d

---
+++ Appjam/Assets/02_Scripts/Mob/Boomer.cs
@@ -0,0 +1,27 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomer : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            animator.SetTrigger("Boom");
+            transform.DOShakePosition(50f, 0.2f).SetEase(Ease.Linear);
+        }
+    }
+    private void Boom()
+    {
+        Destroy(gameObject);
+    }
+}
